064014c94f0314656dc2d4ad686297edad033fe5
qeqs <vadimlygin@yandex.ru>
2016-07-21 18:06:30 +0300

    adding some classes

---
+++ Game/Game/Utils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    class Utils
+    {
+        static public int[] FindPath(int[] from, int[] where, int[,] matrix)
+        {
+            return null;
+        }
+    }
+}
