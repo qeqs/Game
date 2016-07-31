9ea54438f5826cda5de83ae253624c5261eb9ff9
qeqs <vadimlygin@yandex.ru>
2016-08-01 02:12:55 +0300

    Character attributes

--- Game/Game/Objects/Character.cs
+++ Game/Game/Objects/Character.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Game.Objects.Skills;
 using Game.Objects.Characters;
 using Game.Graphics;
+using Game.Strategy;
 
 namespace Game.Objects
 {
@@ -14,6 +15,218 @@ namespace Game.Objects
         private Spell _spell;//TODO: List of spells, factory of list of spells
         private Weapon _weapon;
         private Body _body;
+        private IStrategy strategy;
+
+        #region attributes
+        int agility;
+        int strength;
+        int intelegence;
+        int luck;
+
+        int currentMana;
+        int maxMana;
+
+        int currentHealth;
+        int maxHealth;
+
+        int regenHp;
+        int regenMp;
+
+        double speed;
+
+        int level;//max exp = level*100
+        int experience;
+
+        int damage;
+
+
+        //TODO: дописать свойства 
+        #region attr props
+        public int Agility
+        {
+            get
+            {
+                return agility;
+            }
+
+            set
+            {
+                agility = value;
+                Speed = value * 2;
+            }
+        }
+
+        public int Strength
+        {
+            get
+            {
+                return strength;
+            }
+
+            set
+            {
+                strength = value;
+                MaxHealth = value * 100;
+            }
+        }
+
+        public int Intelegence
+        {
+            get
+            {
+                return intelegence;
+            }
+
+            set
+            {
+                intelegence = value;
+            }
+        }
+
+        public int Luck
+        {
+            get
+            {
+                return luck;
+            }
+
+            set
+            {
+                luck = value;
+            }
+        }
+
+        public int Mana
+        {
+            get
+            {
+                return currentMana;
+            }
+
+            set
+            {
+                currentMana = value;
+            }
+        }
+
+        public int Health
+        {
+            get
+            {
+                return currentHealth;
+            }
+
+            set
+            {
+                currentHealth = value;
+            }
+        }
+        public int MaxHealth
+        {
+            get
+            {
+                return maxHealth;
+            }
+
+            set
+            {
+                maxHealth = value;
+            }
+        }
+        public double Speed
+        {
+            get
+            {
+                return speed;
+            }
+
+            set
+            {
+                speed = value;
+            }
+        }
+
+        public int Level
+        {
+            get
+            {
+                return level;
+            }
+
+            set
+            {
+                level = value;
+            }
+        }
+
+        public int Experience
+        {
+            get
+            {
+                return experience;
+            }
+
+            set
+            {
+                experience = value;
+            }
+        }
+
+        public int Damage
+        {
+            get
+            {
+                return damage;
+            }
+
+            set
+            {
+                damage = value;
+            }
+        }     public int MaxMana
+        {
+            get
+            {
+                return maxMana;
+            }
+
+            set
+            {
+                maxMana = value;
+            }
+        }
+
+        public int RegenHp
+        {
+            get
+            {
+                return regenHp;
+            }
+
+            set
+            {
+                regenHp = value;
+            }
+        }
+
+        public int RegenMp
+        {
+            get
+            {
+                return regenMp;
+            }
+
+            set
+            {
+                regenMp = value;
+            }
+        }
+
+        #endregion
+        #endregion
+        #region static methods
+        //Хотел что-то важное тут написать, отвелкся и забыл :(
+        #endregion
 
         public Animation[] AnimationArray
         {
@@ -25,8 +238,21 @@ namespace Game.Objects
                 };
             }
         }
-        public int Health { get; set; }
 
+        public IStrategy Strategy
+        {
+            get
+            {
+                return strategy;
+            }
+
+            set
+            {
+                strategy = value;
+            }
+        }
+
+   
         public Character(AbstractCharacter character)
         {
             _spell = character.CreateSpell();
@@ -41,6 +267,11 @@ namespace Game.Objects
         public void Hit()
         {
             _weapon.Hit();
+        }
+
+        public void Update()
+        {
+            strategy.Update();
         }
     }
 }
